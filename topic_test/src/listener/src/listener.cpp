2c01c929086088a761433c12552a72b1c6e55e92
LEE_XX <97842693+balabala789654@users.noreply.github.com>
2023-08-14 20:20:14 +0800

    update

--- topic_test/src/listener/src/listener.cpp
+++ topic_test/src/listener/src/listener.cpp
@@ -4,12 +4,16 @@
 class listener : public rclcpp::Node{
 
     public:
+        // 构造函数,有一个参数为节点名称
         listener(std::string name):Node(name){
             RCLCPP_INFO(this->get_logger(), "listener is working");
+            // 创建一个订阅者订阅话题
             listener_sub = this->create_subscription<std_msgs::msg::String>("aaa", 10, std::bind(&listener::sub_callback, this, std::placeholders::_1));
         }
     private:
+        // 声明一个订阅者
         rclcpp::Subscription<std_msgs::msg::String>::SharedPtr listener_sub;
+        // 收到话题数据的回调函数
         void sub_callback(const std_msgs::msg::String::SharedPtr massage){
             RCLCPP_INFO(this->get_logger(), "listener rec msg: %s", massage->data.c_str());
         }
