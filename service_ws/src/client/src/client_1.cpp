8f806da5e9a6c67058c395d2c1ca17c658650155
LEE_XX <97842693+balabala789654@users.noreply.github.com>
2023-09-06 15:01:04 +0800

    update

--- service_ws/src/client/src/client_1.cpp
+++ service_ws/src/client/src/client_1.cpp
@@ -5,23 +5,31 @@ class server_client : public rclcpp::Node
 {
     public:
         server_client(std::string name) : Node(name){   
+            // 构造函数,有一个参数为节点名称
             RCLCPP_INFO(this->get_logger(), "server_client node start");
+            // 创建客户端
+            client = this->create_client<example_interfaces::srv::AddTwoInts>("service_server");
         }
         void send_request(int a, int b){
             RCLCPP_INFO(this->get_logger(), "client_request");
+            // 1.等待服务端上线
             while(!client->wait_for_service(std::chrono::seconds(1))){
+                //等待时检测rclcpp的状态
                 if(!rclcpp::ok()){
-                    RCLCPP_INFO(this->get_logger(), "broken");\
+                    RCLCPP_INFO(this->get_logger(), "broken");
                     return ;
                 }
                 RCLCPP_INFO(this->get_logger(), "waitting");
             }
+            // 2.构造请求的
             auto request = std::make_shared<example_interfaces::srv::AddTwoInts_Request>();
             request->a = a;
             request->b = b;
+            // 3.发送异步请求，然后等待返回，返回时调用回调函数
             client->async_send_request(request, std::bind(&server_client::service_client_callback, this, std::placeholders::_1));
         }
     private:
+        // 声明客户端
         rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client;
         void service_client_callback(rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedFuture result_future){
             auto response = result_future.get();
@@ -34,15 +42,17 @@ int main(int argc, char *argv[])
 {
     /* code */
     rclcpp::init(argc, argv);
+    /*创建对应节点的共享指针对象*/
     auto node = std::make_shared<server_client>("client");
-    
-    while (rclcpp::ok(     ))
-    {
-        /* code */
-        node->send_request(1, 2);
-    }
-    
-    rclcpp::spin(node);
+    /* 运行节点，并检测退出信号*/
+    rclcpp::Rate r(1);
+    int a=3; int b=6;
 
+    while (rclcpp::ok())
+    {
+        node->send_request(a++, b++);
+        r.sleep();
+    }
+    rclcpp::spin(node);
     return 0;
 }
--- service_ws/src/server/src/server_1.cpp
+++ service_ws/src/server/src/server_1.cpp
@@ -4,13 +4,17 @@
 class server : public rclcpp::Node{
 
     public:
+        // 构造函数,有一个参数为节点名称
         server(std::string name):Node(name){
             RCLCPP_INFO(this->get_logger(), "server node start");
+            // 创建服务
             service_server = this->create_service<example_interfaces::srv::AddTwoInts>("service_server", std::bind(&server::server_handle, this, std::placeholders::_1, std::placeholders::_2));
         }
 
     private:
+         // 声明一个服务
         rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service_server;
+        // 收到请求的处理函数
         void server_handle(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request, 
                             std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response){
             RCLCPP_INFO(this->get_logger(), "request %ld %ld", request->a, request->b);
@@ -23,8 +27,14 @@ int main(int argc, char *argv[])
 {
     /* code */
     rclcpp::init(argc, argv);
+    /*创建对应节点的共享指针对象*/
     auto node = std::make_shared<server>("server");
+    /* 运行节点，并检测退出信号*/
     rclcpp::spin(node);
+    // while (rclcpp::ok())
+    // {
+    //     /* code */
+    // }
     
     return 0;
 }
