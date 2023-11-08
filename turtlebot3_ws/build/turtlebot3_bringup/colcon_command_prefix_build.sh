b719dc32f92f2776e5a3fea535a2c7352e4d51d9
LEE_XX <97842693+balabala789654@users.noreply.github.com>
2023-11-08 21:37:47 +0800

    update

---
+++ turtlebot3_ws/build/turtlebot3_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/lx/ros2/ros/turtlebot3_ws/install/turtlebot3_description/share/turtlebot3_description/package.sh"
+. "/home/lx/ros2/ros/turtlebot3_ws/install/turtlebot3_node/share/turtlebot3_node/package.sh"
---
+++ turtlebot3_ws/build/turtlebot3_example/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/lx/ros2/ros/turtlebot3_ws/install/turtlebot3_example'
